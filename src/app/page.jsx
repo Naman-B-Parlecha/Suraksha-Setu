0db4cb071488ae6a9fef373724f805a392cb26a8
mohit-nagaraj <mohitnagaraj20@gmail.com>
2024-08-11 02:15:09 +0530

    new api

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,34 @@
 "use client";
 
+import NewsModal from "@/components/NewsModal";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const Page = () => {
-  
-  return <>MAIN PAGE</>;
+  const [data,setData] = useState([]);
+
+  const getNews = async () => {
+    const res = await axios.get("https://newsapi.org/v2/top-headlines?country=in&apiKey=b7a0ce9a02ba46a389e03d60e25fbe6a");
+    console.log('data',res.data.articles);
+    setData(res.data.articles);
+  }
+
+  useEffect(() => {
+    getNews();
+  },[]);
+
+  return (
+    <div className="flex h-full">
+      <div className="w-1/2">
+        left
+      </div>
+      <div className="w-1/2">
+        <div className="flex flex-col gap-2 max-h-full overflow-auto custom-overflow w-max pr-4">
+        {data.map((article,i) => <NewsModal key={i} article={article} />)}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/components/NewsModal.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { MoveUpRight } from "lucide-react";
+
+const NewsModal = ({ article }) => {
+  const { source, title, description, url, publishedAt } = article;
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+  };
+
+  const images = [
+    'https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638886301/EducationHub/photos/lightning-bolts.jpg',
+    'https://images.pexels.com/photos/1118873/pexels-photo-1118873.jpeg',
+    'https://images.pexels.com/photos/76969/cold-front-warm-front-hurricane-felix-76969.jpeg',
+    'https://images.pexels.com/photos/1446076/pexels-photo-1446076.jpeg',
+    'https://images.pexels.com/photos/1183099/pexels-photo-1183099.jpeg',
+    'https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg',
+    'https://images.pexels.com/photos/2422497/pexels-photo-2422497.jpeg',
+  ]
+
+  const randomImage = images[Math.floor(Math.random() * images.length)];
+
+  return (
+    <div className="max-w-2xl w-full lg:flex relative">
+      <a href={url} target="_blank" rel="noreferrer">
+        <button className="p-1 border-2 rounded-full absolute top-2 right-2">
+          <MoveUpRight size={15}/>
+        </button>
+      </a>
+      <div
+        className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
+        style={{
+          backgroundImage: `url(${randomImage})`,
+        }}
+        title="Woman holding a mug"
+      ></div>
+      <div className="border-r border-b border-l border-grey-light lg:border-l-0 lg:border-t lg:border-grey-light bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+        <div className="mb-8">
+          <div className="text-black font-bold text-xl mb-2">{title}</div>
+          <p className="text-grey-darker text-base">{description}</p>
+        </div>
+        <div className="flex items-center">
+          <div className="text-sm flex justify-between w-full">
+            <p className="text-black leading-none">Source: {source.id}</p>
+            <p className="text-grey-dark">
+              Published: {formatDate(publishedAt)}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewsModal;
