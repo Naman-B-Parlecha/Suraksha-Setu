fc7dc7000420f33ccb232a47fddf4867a011df69
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-08-10 16:29:32 +0530

    nav bar done

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="p-10">
+          <Navbar />
+        </div>
+
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,39 @@
 "use client";
 
-import { Bell ,Settings } from "lucide-react";
+import { IoMdLogIn } from "react-icons/io";
+import { RiLogoutCircleLine } from "react-icons/ri";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
+  const isSidebarCollapsed = false;
+  const isLoggin = false;
   return (
     <div className="flex justify-between items-center w-full mb-7">
       {/* LEFT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <div className="relative">
-          <input
-            type="search"
-            placeholder="Start type to search groups & products"
-            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
-          />
-
-          <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-non">
-            <Bell className="text-gray-500" size={20} />
+          <div
+            className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+              isSidebarCollapsed ? "px-5" : "px-8"
+            }`}
+          >
+            <Image
+              src="/logo.png"
+              alt="logo"
+              width={60}
+              height={60}
+              className="rounded-full"
+            />
+            <h1
+              className={`${
+                isSidebarCollapsed ? "hidden" : "block"
+              } font-extrabold text-2xl`}
+            >
+              Suraksha Setu
+            </h1>
           </div>
         </div>
       </div>
@@ -26,27 +41,34 @@ const Navbar = () => {
       {/* RIGHT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
-          <div></div>
-          <div className="relative">
-            <Bell className="cursor-pointer text-gray-500" size={24} />
-            <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">
-              3
-            </span>
-          </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            <Image
-              src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
-              alt="Profile"
-              width={50}
-              height={50}
-              className="rounded-full h-full object-cover"
-            />
-            <span className="font-semibold">Darshil</span>
+            {isLoggin ? (
+              <>
+                <Image
+                  src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
+                  alt="Profile"
+                  width={50}
+                  height={50}
+                  className="rounded-full h-full object-cover"
+                />
+                <span className="font-semibold">Darshil</span>
+              </>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
         <Link href="/login">
-          Login
+          {isLoggin ? (
+            <>
+              <RiLogoutCircleLine size={30} />
+            </>
+          ) : (
+            <div className="font-bold flex items-center space-x-4 text-lg">
+            Login {"  "} <IoMdLogIn size={30} />
+            </div>
+          )}
         </Link>
       </div>
     </div>
